5770b30000b6b4b315e1b2f1bc900f9f19782aaa
CodeBeginner-Yang <53545582+CodeBeginner-Yang@users.noreply.github.com>
2021-12-23 23:40:32 +0800

    hello push  commit

--- src/main/java/com/yhk/controller/TestGit.java
+++ src/main/java/com/yhk/controller/TestGit.java
@@ -10,6 +10,7 @@ public class TestGit {
         System.out.println("hello Git");
         System.out.println("hello Git2");
         System.out.println("hello yhk");
+        System.out.println("hello push");
 
     }
 }
